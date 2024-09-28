acd616670167ce14145ba1f9e550f292e869bcc8
wiem2002 <wiem.mhedhbi@esprit.tn>
2024-09-28 20:50:28 +0100

    Projet Java

--- src/Animal.java
+++ src/Animal.java
@@ -1,9 +1,9 @@
-
 public class Animal {
-    public String family;
-    public String name ;
-    public int age;
-    public boolean isMammal;
+    private String family;
+    private String name;
+    private int age;
+    private boolean isMammal;
+
 
     public Animal(String family, String name, int age, boolean isMammal) {
         this.family = family;
@@ -11,6 +11,7 @@ public class Animal {
         this.age = age;
         this.isMammal = isMammal;
     }
+
 
     public void displayAnimal() {
         System.out.println("----- Animal Information -----");
@@ -20,9 +21,20 @@ public class Animal {
         System.out.println("Is Mammal: " + this.isMammal);
     }
 
+    @Override
     public String toString() {
         return "Family: " + this.family + ", Name: " + this.name + ", Age: " + this.age + ", Is Mammal: " + this.isMammal;
     }
 
-
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Animal animal = (Animal) obj;
+        return this.name.equals(animal.name);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,55 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Création d'un objet Animal (lion)
-        Animal lion = new Animal("Felidae", "Lion", 5, true);
 
-        // Création d'un objet Zoo (myZoo)
-        Zoo myZoo = new Zoo("Safari Park", "Tunis", 10);
+        Animal lion = new Animal("cats", "Lion", 20, true);
+        Animal giraffe = new Animal("mammals", "Giraffe", 15, true);
+        Animal tiger = new Animal("cats", "Tiger", 5, true);
 
-       Animal[] animals = new Animal[25];
 
-        myZoo.displayZoo();
+        Zoo myZoo = new Zoo("Safari Zoo", "Tunis");
 
-        System.out.println(myZoo);
-        System.out.println(myZoo.toString());
+        System.out.println("Ajout Lion: " + myZoo.addAnimal(lion));
+        System.out.println("Ajout Giraffe: " + myZoo.addAnimal(giraffe));
+        System.out.println("Ajout Lion (encore): " + myZoo.addAnimal(lion)); // Should not add
+        System.out.println("Ajout Tiger: " + myZoo.addAnimal(tiger));
 
-        lion.displayAnimal();
 
-        System.out.println(lion);
-        System.out.println(lion.toString());
+        myZoo.displayAnimals();
+
+
+        int giraffeIndex = myZoo.searchAnimal(giraffe);
+        System.out.println("Recherche de giraffe: " + giraffeIndex);
+
+
+        Animal anotherLion = new Animal("cats", "Lion", 20, true);
+        int anotherLionIndex = myZoo.searchAnimal(anotherLion);
+        System.out.println("Recherche d'un autre Lion identique: " + anotherLionIndex);
+
+
+        Zoo myZoo1 = new Zoo("Safari Zoo", "Tunis");
+        Zoo myZoo2 = new Zoo("Wildlife Park", "Sousse");
+
+        myZoo1.addAnimal(new Animal("Félins", "Lion", 5, true));
+        myZoo1.addAnimal(new Animal("Ruminants", "Girafe", 7, true));
+        myZoo1.addAnimal(new Animal("Oiseaux", "Aigle", 6, true));
+
+        myZoo2.addAnimal(new Animal("Cétacés", "Dauphin", 4, true));
+        myZoo2.addAnimal(new Animal("Félins", "Tigre", 6, true));
+        myZoo2.addAnimal(new Animal("Oiseaux", "Aigle", 3, true));
+        myZoo2.addAnimal(new Animal("Félins", "Lion", 2, true));
+
+        Zoo largerZoo = Zoo.comparerZoo(myZoo1, myZoo2);
+        if (largerZoo != null) {
+            System.out.println("Le zoo avec le plus d'animaux est: " + largerZoo);
+        } else {
+            System.out.println("Les deux zoos ont le même nombre d'animaux.");
+        }
+
+
+        boolean removedLion = myZoo.removeAnimal(lion);
+        System.out.println("Suppression du Lion: " + removedLion);
     }
 }
